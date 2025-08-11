f3c3df0fe7a0a6674f02dc5db10294d3042d4e75
Ludovic Barre <ludovic.barre@foss.st.com>
2025-08-11 13:32:13 +0200

    add simply firmware loadable in RETRAM
    Implement the LP-firmware, the firmware in retention memory (RETRAM)
    used for STM32MP2 cortex-M33 low power modes, including Standby.
    
    This first commit is only empty firmware, build-able and loadable as a
    part of the PM partition of TF-M.
    
    Change-Id: I8392a428ef6a424818bda39b3109e503550e7b07
    Signed-off-by: Simeon Marijon <simeon.marijon@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/stm32mp2-low-power/+/481687
    Tested-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>

---
+++ src/main.c
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * @file    src/main.c
+  * @author  MCD Application Team
+  * @brief   Main program body of Cortex-M33 Low-Power firmware
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#include <stdint.h>
+
+int main(void)
+{
+    return -1;
+}
