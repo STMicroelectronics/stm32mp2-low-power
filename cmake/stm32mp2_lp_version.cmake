49ca26f1a974cce3060998b0b2a92a3b546892bd
Patrick Delaunay <patrick.delaunay@foss.st.com>
2025-12-18 14:08:58 +0100

    Check GIT support in CMake for version management
    The management version of low power firmware is based on GIT but
    it is working only if git tool is available.
    
    Use the package GIT instead to directly call git command and remove
    fatal error when version is not available from Git repository.
    
    Change-Id: I2ed35cdd8bfa616286b161e767b291afea1719d9
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/stm32mp2-low-power/+/519462

--- cmake/stm32mp2_lp_version.cmake
+++ cmake/stm32mp2_lp_version.cmake
@@ -5,21 +5,24 @@
 # The '_VERSION_MANUAL' is used for fallback when Git tags are not available
 set(STM32MP2_LP_VERSION_MANUAL "0.4.0")
 
-execute_process(COMMAND git describe --tags --always
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_VARIABLE STM32MP2_LP_VERSION_FULL
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+find_package(Git)
+if(Git_FOUND)
+    execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --always --match "v[0-9]*\.[0-9]*\.[0-9]*"
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        OUTPUT_VARIABLE STM32MP2_LP_VERSION_FULL
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 # In a repository cloned with --no-tags option STM32MP2_LP_VERSION_FULL will be a hash
 # only hence checking it for a tag format vX.Y.Z to accept as valid version.
 
-string(REGEX MATCH "^v[0-9]+\\.[0-9]+\\.[0-9]+" STM32MP2_LP_VERSION ${STM32MP2_LP_VERSION_FULL})
-string(REGEX REPLACE "v" "" STM32MP2_LP_VERSION ${STM32MP2_LP_VERSION})
+    string(REGEX MATCH "^v[0-9]+\\.[0-9]+\\.[0-9]+" STM32MP2_LP_VERSION "${STM32MP2_LP_VERSION_FULL}")
+    string(REGEX REPLACE "v" "" STM32MP2_LP_VERSION "${STM32MP2_LP_VERSION}")
+endif()
 
 if (STM32MP2_LP_VERSION STREQUAL "")
     set(STM32MP2_LP_VERSION_FULL "v${STM32MP2_LP_VERSION_MANUAL}")
     set(STM32MP2_LP_VERSION "${STM32MP2_LP_VERSION_MANUAL}")
-    message(FATAL_ERROR "Actual STM32MP LP firmware version is not available from Git repository. Settled to " ${STM32MP2_LP_VERSION_FULL})
+    message(WARNING "Actual STM32MP2 LP firmware version is not available from Git repository. Settled to " ${STM32MP2_LP_VERSION_FULL})
 endif()
 
 # Check that manually set version is up to date
@@ -27,4 +30,4 @@ if (NOT STM32MP2_LP_VERSION_MANUAL STREQUAL STM32MP2_LP_VERSION)
     message(WARNING "STM32MP2_LP_VERSION_MANUAL mismatches to actual STM32MP2 LP firmware version. Please update STM32MP2_LP_VERSION_MANUAL in cmake/version.cmake")
 endif()
 
-message(WARNING "version: ${STM32MP2_LP_VERSION_FULL}")
+message(STATUS "STM32MP2 LP firmware version: ${STM32MP2_LP_VERSION_FULL}")
